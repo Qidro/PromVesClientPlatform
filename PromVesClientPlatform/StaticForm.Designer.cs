a938296a1dbd0f0528c63805038fd3727d2fbaa6
Igh <pavlovilyaj1@gmail.com>
2026-09-10 16:44:28 +0500

    Создана форма

---
+++ PromVesClientPlatform/StaticForm.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace PromVesClientPlatform
+{
+    partial class StaticForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StaticForm));
+            formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            lblConnectScale = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button2 = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            formsPlot1.Location = new Point(8, 99);
+            formsPlot1.Name = "formsPlot1";
+            formsPlot1.Size = new Size(523, 469);
+            formsPlot1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(570, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(80, 122);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.Location = new Point(27, 35);
+            button1.Name = "button1";
+            button1.Size = new Size(276, 39);
+            button1.TabIndex = 7;
+            button1.Text = "Начать взвешивание";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // lblConnectScale
+            // 
+            lblConnectScale.AutoSize = true;
+            lblConnectScale.BackColor = Color.Red;
+            lblConnectScale.Font = new Font("Segoe UI", 15F);
+            lblConnectScale.ForeColor = SystemColors.Control;
+            lblConnectScale.Location = new Point(321, 35);
+            lblConnectScale.Name = "lblConnectScale";
+            lblConnectScale.Size = new Size(210, 28);
+            lblConnectScale.TabIndex = 41;
+            lblConnectScale.Text = "Соединение с весами";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(621, 151);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 21);
+            label1.TabIndex = 42;
+            label1.Text = "Группа животных";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(670, 186);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 21);
+            label2.TabIndex = 43;
+            label2.Text = "Отделение";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.Location = new Point(690, 221);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 21);
+            label3.TabIndex = 44;
+            label3.Text = "Бригада";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(522, 256);
+            label4.Name = "label4";
+            label4.Size = new Size(236, 21);
+            label4.TabIndex = 45;
+            label4.Text = "За кем прикреплены животные";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F);
+            label5.Location = new Point(537, 291);
+            label5.Name = "label5";
+            label5.Size = new Size(221, 21);
+            label5.TabIndex = 46;
+            label5.Text = "Номер животного или станка";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F);
+            label6.Location = new Point(621, 326);
+            label6.Name = "label6";
+            label6.Size = new Size(137, 21);
+            label6.TabIndex = 47;
+            label6.Text = "Количество голов";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.Location = new Point(764, 326);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(270, 29);
+            textBox1.TabIndex = 48;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 12F);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(764, 151);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(270, 29);
+            comboBox1.TabIndex = 49;
+            // 
+            // comboBox2
+            // 
+            comboBox2.Font = new Font("Segoe UI", 12F);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(764, 256);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(270, 29);
+            comboBox2.TabIndex = 50;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(764, 186);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(270, 29);
+            textBox2.TabIndex = 51;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F);
+            textBox3.Location = new Point(764, 221);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(270, 29);
+            textBox3.TabIndex = 52;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F);
+            textBox4.Location = new Point(764, 291);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(270, 29);
+            textBox4.TabIndex = 53;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F);
+            button2.Location = new Point(636, 361);
+            button2.Name = "button2";
+            button2.Size = new Size(398, 46);
+            button2.TabIndex = 54;
+            button2.Text = "Сохранить вес";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(647, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(80, 122);
+            pictureBox2.TabIndex = 55;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(724, 12);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(80, 122);
+            pictureBox3.TabIndex = 56;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(801, 12);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(80, 122);
+            pictureBox4.TabIndex = 57;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(877, 12);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(80, 122);
+            pictureBox5.TabIndex = 58;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(954, 12);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(80, 122);
+            pictureBox6.TabIndex = 59;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(641, 413);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(404, 155);
+            pictureBox7.TabIndex = 60;
+            pictureBox7.TabStop = false;
+            // 
+            // StaticForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1057, 568);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(button2);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lblConnectScale);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(formsPlot1);
+            Font = new Font("Segoe UI", 9F);
+            Name = "StaticForm";
+            Text = "StaticForm";
+            Load += StaticForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Label lblConnectScale;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button2;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+    }
+}
